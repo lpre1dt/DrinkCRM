4180971e19fc5cd79884858573e3a84c1207b9ac
lukaspreidt <lpreidt@gmail.com>
2021-10-20 11:46:54 +0200

    Second Commit

--- DrinkCRM/src/Main.java
+++ DrinkCRM/src/Main.java
@@ -3,7 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		for(int i = 0 ; i<100 ; i++) {
 System.out.println("Hello World");
+		}
 	}
 
 }
